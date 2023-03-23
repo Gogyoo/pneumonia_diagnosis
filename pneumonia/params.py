c0eec61e30b477e8b58d52fab94207e8cfc67366
Jerome Petit <pourriel1@hotmail.com>
2023-03-23 22:40:56 +0100

    Skeleton code for import/export of fitted models

--- pneumonia/params.py
+++ pneumonia/params.py
@@ -5,6 +5,8 @@ DATA_SOURCE = os.environ["DATA_SOURCE"]
 FITTING_TARGET = os.environ["FITTING_TARGET"]
 MODEL_TARGET = os.environ["MODEL_TARGET"]
 DATA_RATIO = 1
+DATA_BUCKET_NAME = os.environ["DATA_BUCKET_NAME"]
+DATA_BUCKET_URI = os.environ["DATA_BUCKET_URI"]
 
 LOCAL_DATA_PATH = "raw_data"
 LOCAL_REGISTRY_PATH =  "pneumonia/training_weights"
--- pneumonia/registry.py
+++ pneumonia/registry.py
@@ -1,1 +1,51 @@
-pass
+
+
+from pneumonia.params import *
+
+def save_results():
+    """Takes a model's results (its parameters and metrics, from history)
+    and dumps them separately in a timestamped pickle file.
+    Locally: TODO: in which subfolder?
+    Remotely: one bucket for params, another for metrics.
+    Each pickle goes to a blob."""
+
+    if MODEL_TARGET == "local":
+        print("THIS IS A TEST: Saving results locally...")
+
+    elif MODEL_TARGET == "gcp":
+        print("THIS IS A TEST: Saving results on the cloud...")
+
+    else:
+        print ("Locale for results should be\
+            either \'local\' or \'\gcp'.\
+            Please check in .env.")
+
+
+
+def save_model():
+    """Takes a model's weights and dumps them in a timestamped pickle file.
+    Locally: TODO: in which subfolder?
+    Remotely: Each pickle goes to a blob."""
+
+    if MODEL_TARGET == "local":
+        print("THIS IS A TEST: Saving model weights locally...")
+    elif MODEL_TARGET == "gcp":
+        print("THIS IS A TEST: Saving model weights on the cloud...")
+    else:
+        print ("Locale to export a model should be\
+            either \'local\' or \'\gcp'.\
+            Please check in .env.")
+
+
+
+def load_model():
+    """Loads a model's weights saved locally or in a blob."""
+
+    if MODEL_TARGET == "local":
+        print("THIS IS A TEST: Loading model weights locally...")
+    elif MODEL_TARGET == "gcp":
+        print("THIS IS A TEST: Loading model weights on the cloud...")
+    else:
+        print ("Locale to load a model should be\
+            either \'local\' or \'\gcp'.\
+             Please check in .env.")
