ccfad92ed305847940293f4cd65fdb5e1bb0a586
Jerome Petit <pourriel1@hotmail.com>
2023-03-23 17:51:35 +0100

    modified file structure towards cloud training: 2nd step

--- pneumonia/params.py
+++ pneumonia/params.py
@@ -1,1 +1,10 @@
-pass
+import os
+
+
+DATA_SOURCE = os.environ["DATA_SOURCE"]
+FITTING_TARGET = os.environ["FITTING_TARGET"]
+MODEL_TARGET = os.environ["MODEL_TARGET"]
+DATA_RATIO = 1
+
+LOCAL_DATA_PATH = "raw_data"
+LOCAL_REGISTRY_PATH =  "pneumonia/training_weights"
