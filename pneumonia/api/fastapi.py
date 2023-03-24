acf9f0022a58f2d0a5dfdda5f75957054e850915
Jerome Petit <pourriel1@hotmail.com>
2023-03-24 12:15:02 +0100

    Step 1 - empty Dockerfile

--- pneumonia/api/fastapi.py
+++ pneumonia/api/fastapi.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from pneumonia.baseline import predict
 
 app = FastAPI()
 
@@ -6,9 +7,20 @@ app = FastAPI()
 def index():
     return {"OK":True}
 
+
+
 @app.get('/predict')
-# input: X-ray image
+# input: X-ray image, or nothing, and we
+# tell the function to sample randomly
+# from the test subset
+
 # output: Normal/Negative or Pneumonia/Positive,
 # perhaps even with confidence of the model attached to this result
+
+
 def predict(image):
+    # convert image to jpeg if provided
+    # turn it into a tfds
+    # load our best model
+    # predict method on that image
     return {'wait': "Diagnostic: Pneumonia"}
