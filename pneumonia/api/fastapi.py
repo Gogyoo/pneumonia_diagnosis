a1887611b6ef2016d17a4d17ec19b6c0288a5ee4
Jerome Petit <pourriel1@hotmail.com>
2023-03-24 11:35:04 +0100

    Step 1 - empty Dockerfile

--- pneumonia/api/fastapi.py
+++ pneumonia/api/fastapi.py
@@ -5,3 +5,10 @@ app = FastAPI()
 @app.get("/")
 def index():
     return {"OK":True}
+
+@app.get('/predict')
+# input: X-ray image
+# output: Normal/Negative or Pneumonia/Positive,
+# perhaps even with confidence of the model attached to this result
+def predict(image):
+    return {'wait': "Diagnostic: Pneumonia"}
