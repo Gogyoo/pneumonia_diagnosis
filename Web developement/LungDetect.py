0b61be6a9ce3ad000706483fc8f78713988d7601
Anya Pasko <annapasko9889@gmail.com>
2023-03-24 10:30:22 +0000

    Added the updated efficient net model, as well as Web Dev file and a couple other notebooks that I havent added earlier.

---
+++ Web developement/LungDetect.py
@@ -0,0 +1,9 @@
+import streamlit as st
+st.set_page_config(page_title='Welcome to LungDetect', page_icon=':sparkles:',
+                   layout='wide')
+
+# Header section
+
+st.subheader('We are making lung diseases detection easier')
+st.write('Just upload an image of your chest X-ray and get a result in less than a minute')
+st.write('[Learn more >]'('link to our github/some research papers?'))
