81d338dbe01f98e4898da64974bb8667dc87c4d4
Anya Pasko <annapasko9889@gmail.com>
2023-03-27 14:42:17 +0100

    Draft version of the website

--- Web developement/streamlit.py
+++ Web developement/streamlit.py
@@ -2,6 +2,8 @@ import streamlit as st
 import json
 import requests
 from streamlit_lottie import st_lottie
+import base64
+from pathlib import Path
 
 # Defining the functions
 
@@ -23,52 +25,61 @@ st.set_page_config(page_title='Welcome to LungDetect', page_icon=':sparkles:',
                    layout='wide')
 
 # Header section
-
-with st.container():
-    st.header('Welcome to LungDetect :sparkles:')
+col1, col2 = st.columns((2, 1))
+with col1:
+    with st.container():
+        st.header('Welcome to LungDetect :sparkles:')
 
 # Subheader
+with col2:
+    def img_to_bytes(img_path):
+        img_bytes = Path(img_path).read_bytes()
+        encoded = base64.b64encode(img_bytes).decode()
+        return encoded
 
+    def img_to_html(img_path):
+        img_html = "<img src='data:image/png;base64,{}' class='img-fluid'>".format(
+        img_to_bytes(img_path)
+        )
+        return img_html
+
+    st.markdown("<p style='text-align: center ; color: grey;'>"+
+                img_to_html('Web developement/88293-lungs.gif')
+                +"</p>", unsafe_allow_html=True)
+# About us and our project
+
+# Add text to the left column
 with st.container():
     st.subheader('We are making lung diseases detection easier')
     st.write('Just upload an image of your chest X-ray and get a result in less than a minute!')
     # st.write('[Learn more >]link to our github/some research papers?')
 
-# About us and our project
+# Add text to the left column
 
 with st.container():
     st.write('---')
-    left_column, right_column = st.columns(2)
-    with left_column:
-        st.header('Overview')
-        st.write('##')
-        st.write('''
-                 Pneumonia is an infection that inflames the air sacs in one or both lungs. The air sacs may fill with fluid or pus (purulent material), causing cough with phlegm or pus, fever, chills, and difficulty breathing. A variety of organisms, including bacteria, viruses and fungi, can cause pneumonia.
+    st.header('Overview')
+    st.write('##')
+    st.write('''
+            Pneumonia is an infection that inflames the air sacs in one or both lungs. The air sacs may fill with fluid or pus (purulent material), causing cough with phlegm or pus, fever, chills, and difficulty breathing. A variety of organisms, including bacteria, viruses and fungi, can cause pneumonia.
 
-                Pneumonia can range in seriousness from mild to life-threatening. It is most serious for infants and young children, people older than age 65, and people with health problems or weakened immune systems.
-                 ''')
+            Pneumonia can range in seriousness from mild to life-threatening. It is most serious for infants and young children, people older than age 65, and people with health problems or weakened immune systems.
+            ''')
 
+
+# Next section of the website
 with st.container():
     st.write('---')
-    left_column, right_column = st.columns(2)
-    with left_column:
-        st.header('The purpose of LungDetect')
-        st.write('##')
-        st.write('''
-                 The diagnosis of pneumonia requires a review of chest radiography (CXR) by a highly qualified specialist, laboratory tests, vital signs, and clinical history, which makes its detection a difficult task. It normally presents as an area of increased opacity within the CXR. Even so, the identification of the diagnosis of pneumonia is complex due to other pulmonary conditions such as hemorrhages, lung cancer, postsurgical changes, pulmonary edema, atelectasis, or collapse. The comparison of CRX performed at different times and the relationship with the clinical history is essential for diagnosis.
+    st.header('The purpose of LungDetect')
+    st.write('##')
+    st.write('''
+            The diagnosis of pneumonia requires a review of chest radiography (CXR) by a highly qualified specialist, laboratory tests, vital signs, and clinical history, which makes its detection a difficult task. It normally presents as an area of increased opacity within the CXR. Even so, the identification of the diagnosis of pneumonia is complex due to other pulmonary conditions such as hemorrhages, lung cancer, postsurgical changes, pulmonary edema, atelectasis, or collapse. The comparison of CRX performed at different times and the relationship with the clinical history is essential for diagnosis.
 
-                This project seeks to design an application for the automatic detection of pneumonia, which may be appropriate in regions where health professionals do not arrive or as support when defining a diagnosis to reduce the mortality rate associated with Pneumonia.
-                 ''')
+            This project seeks to design an application for the automatic detection of pneumonia, which may be appropriate in regions where health professionals do not arrive or as support when defining a diagnosis to reduce the mortality rate associated with Pneumonia.
+            ''')
 
 
-# Inserting an image of lungs
+uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
 
-lottie_coding = load_lottieurl('https://assets4.lottiefiles.com/packages/lf20_0l21klz3.json')
-# hello_lottie = load_lottieurl('https://assets4.lottiefiles.com/packages/lf20_0l21klz3.json')
-st_lottie(lottie_coding, speed = 1)
 
-# Use st_css to change the size and position of the animation
-st.write('<style>div.WidgetBox.stLottie{width:50px !important;height:50px !important;margin:0 auto;}</style>', unsafe_allow_html=True)
-
-# Section where a user can drop an image and get a result
-uploaded_file = st.file_uploader("Choose a chest X-ray image", type=["jpg", "jpeg", "png"])
+st.success('The result is ready!', icon="✅")
