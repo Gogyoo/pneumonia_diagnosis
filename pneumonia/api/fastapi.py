f9135b5fca16d0d940f046682e1ed484e5fa1dd4
Jerome Petit <pourriel1@hotmail.com>
2023-03-28 11:41:51 +0200

    Basic prediction displays, pre-trained model might have a preprocessing bug

--- pneumonia/api/fastapi.py
+++ pneumonia/api/fastapi.py
@@ -1,8 +1,24 @@
 from fastapi import FastAPI, UploadFile, File
-from tensorflow.keras.utils import load_img
-from tensorflow.keras import Model
+import numpy as np
+
+from google.cloud import storage
+from pneumonia.params import *
+
+from tensorflow.keras import models
+from tensorflow.io import decode_image
+from tensorflow.image import resize
+from tensorflow import reshape
 
 app = FastAPI()
+
+client = storage.Client()
+
+bloblist = client.list_blobs(MODEL_BUCKET_NAME)
+latest_model_path_to_save = os.path.join(LOCAL_REGISTRY_PATH, "model.h5")
+for blob in bloblist:
+    blob.download_to_filename(latest_model_path_to_save)
+pre_trained = models.load_model(latest_model_path_to_save)
+
 
 @app.get("/")
 def index():
@@ -11,16 +27,19 @@ def index():
 
 
 @app.post('/predict')
-# input: X-ray image, or nothing, and we
-# tell the function to sample randomly
-# from the test subset
+# input: X-ray image
 
 # output: Normal/Negative or Pneumonia/Positive,
 # perhaps even with confidence of the model attached to this result
 
-async def detect_faces(img: UploadFile=File(...)):
+async def predict(img: UploadFile=File(...)):
     # Receiving and decoding the image
     tested = await img.read()
-    pre_trained = Model.load_weights("gs://pneumonia-models/model.h5")
-    verdict = pre_trained.predict(tested)
-    return {"Prediction": verdict}
+
+    decoded = decode_image(tested, channels=3)
+    resized = resize(decoded, size=(256,256))
+    tupled = reshape(resized, shape=(-1,256,256,3))
+    verdict = pre_trained.predict(tupled)
+
+
+    return {"Prediction": str(verdict[0][0])}
